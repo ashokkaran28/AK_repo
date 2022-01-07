459b493f9f19aca704db75e2a8b6c5c2023f7128
Ashok Kumar Karan <ashokkaran28@gmail.com>
2022-01-06 19:24:36 -0500

    fixed one bug with merging

--- data/src/main/java/client/data/pojo/Customer.java
+++ data/src/main/java/client/data/pojo/Customer.java
@@ -93,11 +93,39 @@ public class Customer {
 	}
 
 	/**
-     * toString method has been overridden in order to better debugging the Customer class
-     */
+	 * toString method has been overridden in order to better debugging the Customer
+	 * class
+	 */
 	@Override
 	public String toString() {
 		return "Customer [sequenceID=" + sequenceID + ", group=" + group + ", address1=" + address1 + ", street="
 				+ street + ", city=" + city + ", zip=" + zip + ", country=" + country + ", clinetID=" + clinetID + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// if both the object references are referring to the same object.
+		if (this == obj)
+			return true;
+
+		// it checks if the argument is of the
+		// type Customer by comparing the classes
+		// of the passed argument and this object.
+		if (obj == null || obj.getClass() != this.getClass())
+			return false;
+
+		// type casting of the argument.
+		Customer cust = (Customer) obj;
+
+		// comparing the state of argument with
+		// the state of 'this' Object.
+		return (this.getStreet().equalsIgnoreCase(cust.getStreet()) && this.getCity().equalsIgnoreCase(cust.getCity())
+				&& this.getZip().equalsIgnoreCase(cust.getZip())
+				&& this.getCountry().equalsIgnoreCase(cust.getCountry()));
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
 	}
 }
